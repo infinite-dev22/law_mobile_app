a3a3659c76b58dc05090d98bba825222485d828a
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-07-29 19:30:14 +0300

    Add global initial widget

---
+++ lib/Global/Widgets/initial_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GlobalInitialWidget extends StatelessWidget {
+  const GlobalInitialWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Initial Widget"),
+    );
+  }
+}
