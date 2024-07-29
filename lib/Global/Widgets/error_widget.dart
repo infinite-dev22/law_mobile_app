fcd5d0c06536e2e04a7e7ecaa39049faacb4ff4e
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-07-29 18:13:08 +0300

    Add basic error page

---
+++ lib/Global/Widgets/error_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GlobalErrorWidget extends StatelessWidget {
+  const GlobalErrorWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("An error occurred"),);
+  }
+}
