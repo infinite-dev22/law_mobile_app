9814b01d64c8a8ec7fdd8ec2ae534a4ea2599462
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-07-29 11:55:38 +0300

    Add colors.dart file

---
+++ lib/Global/Variables/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const shadowColor = Colors.black;
+  static const inActiveColor = Colors.grey;
+  static const darkerColor = Color(0xFF3E4249);
+  static const lighterColor = Color(0xFFF4EEEE);
+  static const appBgColor = Color(0xFFFFFFFF);
+  static const primary = Color(0xFF1467CB);
+  static const secondary = Color(0xFFCBE1F3);
+
+}
