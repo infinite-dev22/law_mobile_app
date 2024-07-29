240c093c782fa7ec6ce9a07704e71033cfade257
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-07-29 18:47:13 +0300

    Add issues page event

--- lib/legal_issues/presentation/bloc/legal_issues_page/legal_issues_pages_event.dart
+++ lib/legal_issues/presentation/bloc/legal_issues_page/legal_issues_pages_event.dart
@@ -1,5 +1,23 @@
 part of 'legal_issues_pages_bloc.dart';
 
-sealed class LegalIssuesPagesEvent extends Equatable {
-  const LegalIssuesPagesEvent();
+sealed class LegalIssuesPageEvent extends Equatable {
+  const LegalIssuesPageEvent();
+
+  @override
+  List<Object?> get props => [];
 }
+
+class LoadLegalIssuesEvent extends LegalIssuesPageEvent {}
+
+class RefreshLegalIssuesEvent extends LegalIssuesPageEvent {}
+
+class LoadSingleLegalIssueEvent extends LegalIssuesPageEvent {
+  final int id;
+
+  const LoadSingleLegalIssueEvent(this.id);
+
+  @override
+  List<Object?> get props => [id];
+}
+
+class LegalIssueAddedEvent extends LegalIssuesPageEvent {}
