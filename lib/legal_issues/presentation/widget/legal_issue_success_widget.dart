e9cef52e257d32e562a03bb64033895287cb1b5b
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-07-30 11:27:41 +0300

    Add LegalIssueItem to LegalIssueSuccessWidget

--- lib/legal_issues/presentation/widget/legal_issue_success_widget.dart
+++ lib/legal_issues/presentation/widget/legal_issue_success_widget.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../bloc/legal_issues_page/legal_issues_pages_bloc.dart';
+import 'legal_issue_item_widget.dart';
+
 class LegalIssueSuccessWidget extends StatelessWidget {
   const LegalIssueSuccessWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer(builder: (context, state) => const Center(
-      child: Text("Legal Issues"),
-    ), listener: (context, state) {
-
-    },);
+    return BlocConsumer<LegalIssuesPagesBloc, LegalIssuesPageState>(
+      builder: (context, state) {
+        return ListView.builder(
+            padding: const EdgeInsets.all(8.0),
+            itemCount: state.issues!.length,
+            itemBuilder: (context, index) => Card(
+                  child: ListTile(
+                    title: LegalIssueItem(data: state.issues!.elementAt(index)),
+                  ),
+                ));
+      },
+      listener: (context, state) {},
+    );
   }
 }
