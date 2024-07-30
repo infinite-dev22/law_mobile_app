65a025d1fa04d4a1360e8d6329577f137255f718
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-07-30 11:47:25 +0300

    Add legal_document_success_widget.dart

---
+++ lib/legal_documents/presentation/widget/legal_document_success_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/legal_documents_page/legal_documents_page_bloc.dart';
+import 'legal_document_item_widget.dart';
+
+class LegalDocumentSuccessWidget extends StatelessWidget {
+  const LegalDocumentSuccessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<LegalDocumentsPageBloc, LegalDocumentsPageState>(
+      builder: (context, state) {
+        return ListView.builder(
+            padding: const EdgeInsets.all(8.0),
+            itemCount: state.documents!.length,
+            itemBuilder: (context, index) => Card(
+                  child: ListTile(
+                    title: LegalDocumentItem(data: state.documents!.elementAt(index)),
+                  ),
+                ));
+      },
+      listener: (context, state) {},
+    );
+  }
+}
