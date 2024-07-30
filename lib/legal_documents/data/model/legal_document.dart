8d4bc8b60a67addbbf03e399ac8fdca73096c610
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-07-30 11:46:14 +0300

    Add legal_documents.dart (entity file)

---
+++ lib/legal_documents/data/model/legal_document.dart
@@ -0,0 +1,15 @@
+class LegalDocument {
+  String issueId;
+  String clientName;
+  String title;
+  String documentTitle;
+  String status;
+
+  LegalDocument(
+    this.issueId,
+    this.clientName,
+    this.title,
+    this.documentTitle,
+    this.status,
+  );
+}
