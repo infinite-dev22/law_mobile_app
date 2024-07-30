6bf801407e4d05e99c5078786c2796035cd2aa26
Jonathan Mark Mwigo <mwigojm@gmail.com>
2024-07-30 13:26:22 +0300

    Add FAB and Modal bottom sheet

--- lib/legal_issues/presentation/pages/legal_issues_page.dart
+++ lib/legal_issues/presentation/pages/legal_issues_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:toast/toast.dart';
 
 import '../../../Global/Widgets/error_widget.dart';
-import '../../../Global/Widgets/initial_widget.dart';
 import '../../../Global/Widgets/loading_widget.dart';
 import '../bloc/legal_issues_page/legal_issues_pages_bloc.dart';
 import '../widget/legal_issue_success_widget.dart';
@@ -15,40 +15,55 @@ class LegalIssuesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     ToastContext().init(context);
 
-    return BlocConsumer<LegalIssuesPagesBloc, LegalIssuesPageState>(
-      builder: (context, state) {
-        if (state.status.isInitial) {
-          context.read<LegalIssuesPagesBloc>().add(LoadLegalIssuesEvent());
-        }
-        if (state.status.isSuccess) {
-          return const LegalIssueSuccessWidget();
-        }
-        if (state.status.isLoading) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(FeatherIcons.plus),
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return const Center(
+                child: Text("Issues Form here"),
+              );
+            },
+          );
+        },
+      ),
+      body: BlocConsumer<LegalIssuesPagesBloc, LegalIssuesPageState>(
+        builder: (context, state) {
+          if (state.status.isInitial) {
+            context.read<LegalIssuesPagesBloc>().add(LoadLegalIssuesEvent());
+          }
+          if (state.status.isSuccess) {
+            return const LegalIssueSuccessWidget();
+          }
+          if (state.status.isLoading) {
+            return const GlobalLoadingWidget();
+          }
+          if (state.status.isError) {
+            return const GlobalErrorWidget();
+          }
           return const GlobalLoadingWidget();
-        }
-        if (state.status.isError) {
-          return const GlobalErrorWidget();
-        }
-        return const GlobalLoadingWidget();
-      },
-      listener: (context, state) {
-        if (state.status.isInitial) {
-          Toast.show("Initial State",
-              duration: Toast.lengthShort, gravity: Toast.bottom);
-        }
-        if (state.status.isSuccess) {
-          Toast.show("Success State",
-              duration: Toast.lengthShort, gravity: Toast.bottom);
-        }
-        if (state.status.isLoading) {
-          Toast.show("Loading State",
-              duration: Toast.lengthShort, gravity: Toast.bottom);
-        }
-        if (state.status.isError) {
-          Toast.show("Error State",
-              duration: Toast.lengthShort, gravity: Toast.bottom);
-        }
-      },
+        },
+        listener: (context, state) {
+          if (state.status.isInitial) {
+            Toast.show("Initial State",
+                duration: Toast.lengthShort, gravity: Toast.bottom);
+          }
+          if (state.status.isSuccess) {
+            Toast.show("Success State",
+                duration: Toast.lengthShort, gravity: Toast.bottom);
+          }
+          if (state.status.isLoading) {
+            Toast.show("Loading State",
+                duration: Toast.lengthShort, gravity: Toast.bottom);
+          }
+          if (state.status.isError) {
+            Toast.show("Error State",
+                duration: Toast.lengthShort, gravity: Toast.bottom);
+          }
+        },
+      ),
     );
   }
 }
