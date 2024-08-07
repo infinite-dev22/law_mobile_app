0863a3d1c95d248fb6a1ee21b89caf9054f9b9cb
infinite-dev22 <mwigojm@gmail.com>
2024-08-07 13:26:08 +0300

    Add new variable: - authData

--- lib/Global/Variables/app_runtime_values.dart
+++ lib/Global/Variables/app_runtime_values.dart
@@ -1,1 +1,5 @@
-late String currentUserToken = "2|REeJCGqER7aIwVfoxCMRGqt4WMgVMBe742ocESkv5e320d84";
+import 'package:dirm_attorneys_mobile/authentication/data/model/login_response_model.dart';
+
+late LoginResponseModel authData;
+late String currentUserToken =
+    "2|REeJCGqER7aIwVfoxCMRGqt4WMgVMBe742ocESkv5e320d84";
