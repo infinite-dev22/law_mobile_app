615bc9ddebe11a6d87bb71cf590c3f0831610e17
infinite-dev22 <mwigojm@gmail.com>
2024-08-09 11:11:35 +0300

    Redesign widget

--- lib/legal_issues/presentation/widget/legal_issue_item_widget.dart
+++ lib/legal_issues/presentation/widget/legal_issue_item_widget.dart
@@ -1,75 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bounceable/flutter_bounceable.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
-import '../../../Global/Variables/colors.dart';
 import '../../data/model/legal_issue.dart';
 
 class LegalIssueItem extends StatelessWidget {
-  const LegalIssueItem({super.key, required this.data});
+  final Function() onTap;
+  final Function() onTapDelete;
+  final Function() onTapDownload;
+
+  const LegalIssueItem({
+    super.key,
+    required this.data,
+    required this.onTap,
+    required this.onTapDelete,
+    required this.onTapDownload,
+  });
 
   final LegalIssue data;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: _buildInfo(),
-        )
-      ],
+    return Bounceable(
+      onTap: onTap,
+      child: _buildBody(),
     );
   }
 
-  Widget _buildInfo() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          data.title ?? "N/A",
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+  Widget _buildBody() {
+    return Card(
+      color: Colors.white10.withOpacity(.2),
+      child: ListTile(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              child: Text(
-                data.uploadedFileName ?? "N/A",
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  fontSize: 12,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  data.title ?? "N/A",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                      fontSize: 14, fontWeight: FontWeight.w600),
                 ),
-              ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  data.uploadedFileName ?? "N/A",
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  data.issueStatus ?? "N/A",
+                  style: const TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  data.createdAt.toString(),
+                  style: const TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: onTapDelete,
+                  icon: const Icon(
+                    FeatherIcons.trash2,
+                    color: Colors.redAccent,
+                  ),
+                ),
+                IconButton(
+                  onPressed: onTapDownload,
+                  icon: const Icon(
+                    FeatherIcons.download,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
-        const SizedBox(
-          height: 5,
-        ),
-        Text(
-          data.issueStatus ?? "N/A",
-          style: const TextStyle(
-            fontSize: 13,
-            color: AppColors.primary,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        Text(
-          data.createdAt.toString(),
-          style: const TextStyle(
-            fontSize: 13,
-            color: AppColors.primary,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
