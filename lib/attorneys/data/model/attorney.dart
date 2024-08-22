b0cbe6432a5b85cec7589809a9ad95e72304c3c8
infinite-dev22 <mwigojm@gmail.com>
2024-08-22 13:14:31 +0300

    Add Attorneys

--- lib/attorneys/data/model/attorney.dart
+++ lib/attorneys/data/model/attorney.dart
@@ -1,15 +1,90 @@
-class Attorney {
-  String issueId;
-  String clientName;
-  String title;
-  String documentTitle;
-  String status;
+import 'package:equatable/equatable.dart';
 
-  Attorney(
-    this.issueId,
-    this.clientName,
-    this.title,
-    this.documentTitle,
-    this.status,
-  );
+class Attorney extends Equatable {
+  const Attorney({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    required this.emailVerifiedAt,
+    required this.phone,
+    required this.avatar,
+    required this.userType,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  final int? id;
+  final String? name;
+  final String? username;
+  final String? email;
+  final dynamic emailVerifiedAt;
+  final dynamic phone;
+  final dynamic avatar;
+  final dynamic userType;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+
+  Attorney copyWith({
+    int? id,
+    String? name,
+    String? username,
+    String? email,
+    dynamic emailVerifiedAt,
+    dynamic phone,
+    dynamic avatar,
+    dynamic userType,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) {
+    return Attorney(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      emailVerifiedAt: emailVerifiedAt ?? this.emailVerifiedAt,
+      phone: phone ?? this.phone,
+      avatar: avatar ?? this.avatar,
+      userType: userType ?? this.userType,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+    );
+  }
+
+  factory Attorney.fromJson(Map<String, dynamic> json){
+    return Attorney(
+      id: json["id"],
+      name: json["name"],
+      username: json["username"],
+      email: json["email"],
+      emailVerifiedAt: json["email_verified_at"],
+      phone: json["phone"],
+      avatar: json["avatar"],
+      userType: json["user_type"],
+      createdAt: DateTime.tryParse(json["created_at"] ?? ""),
+      updatedAt: DateTime.tryParse(json["updated_at"] ?? ""),
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "name": name,
+    "username": username,
+    "email": email,
+    "email_verified_at": emailVerifiedAt,
+    "phone": phone,
+    "avatar": avatar,
+    "user_type": userType,
+    "created_at": createdAt?.toIso8601String(),
+    "updated_at": updatedAt?.toIso8601String(),
+  };
+
+  @override
+  String toString(){
+    return "$id, $name, $username, $email, $emailVerifiedAt, $phone, $avatar, $userType, $createdAt, $updatedAt, ";
+  }
+
+  @override
+  List<Object?> get props => [
+    id, name, username, email, emailVerifiedAt, phone, avatar, userType, createdAt, updatedAt, ];
 }
--- lib/attorneys/data/repository/definition/attorney_repo.dart
+++ lib/attorneys/data/repository/definition/attorney_repo.dart
@@ -2,4 +2,6 @@ import '../../model/attorney.dart';
 
 abstract class AttorneyRepo {
   Future<List<Attorney>> getAllAttorneys(String authToken);
+
+  Future<Attorney?> getAttorney(String authToken, String slug);
 }
